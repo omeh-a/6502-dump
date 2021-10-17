3543110182a14eba3c3bb7907d500eee408a3f7e
omeh-a <omeh-a@users.noreply.github.com>
2021-10-17 13:46:10 +1100

    Added CAD for breadboard couplers and some code for decoder

---
+++ 6502monitor/src/decode.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <
+#include <stdio.h>
+#include "decode.h";
+
+constexpr char ASCII_A_OFFSET = 55;
+constexpr char ASCII_ZERO_OFFSET = 48;
+
+// Given a 6502 instruction as binary, return an information string
+std::string decode(std::byte instr) {
+    std::string s = hexString(instr) + " ## ";
+
+    switch(static_cast<int>(instr)) {
+        case 0x00:
+            s+="BRK | Break";
+        case 0x01:
+            s+="ORA | Break";
+        
+        default:
+            s+=" no definition";
+
+    }
+}
+
+// Returns 
+std::string hexString(std::byte b) {
+    std::string out = "0x";
+    char c1 = charFormat(static_cast<char>(b) % 64);
+    char c2 = charFormat(static_cast<char>(b) >> 4);
+
+    return out + c1 + c2;
+}
+
+// Given a hex digit, return it as a char. e.g. 0xC in, 'C' out.
+char charFormat(char c) {
+    // check for bad input
+    if (c > 16) {
+        return '!';
+    }
+
+    if (c >= 10) {
+        return c + ASCII_A_OFFSET;
+    }
+    return c + ASCII_ZERO_OFFSET;
+}
---
+++ 6502monitor/src/decode.h
@@ -0,0 +1,6 @@
+#ifndef DECODE_H
+#endif
+
+std::string decode(std::byte instr);
+std::string hexString(std::byte b);
+char charFormat(char c);
