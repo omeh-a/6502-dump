84f629f84cf157a1aeb413affa55a014823b5453
omeh-a <omeh-a@users.noreply.github.com>
2021-10-21 17:07:34 +1100

    Refactored to use a linked list rather than an array for buffer. Echo program now works properly.

--- Dynamic programmer/src/main.cpp
+++ Dynamic programmer/src/main.cpp
@@ -7,50 +7,92 @@
 #define EEPROM_D0 5
 #define EEPROM_D7 12
 #define WRITE_EN 13
+#define WRITE_BUTTON 19
 #define FULL_WIPE 1
 #define SEMI_WIPE 0
+#define BUTTON_THRESHOLD 1000
+
+struct ListNode {
+    ListNode *next;
+    uint8_t value;
+};
 
 void purgeBuffer(bool fullWipe);
-char charFormat(char c);
+boolean write();
 
-constexpr size_t EEPROM_SIZE = 262144;
+constexpr unsigned long EEPROM_SIZE = 262144;
 constexpr char ASCII_A_OFFSET = 55;
 constexpr char ASCII_ZERO_OFFSET = 48;
 
-uint8_t *buffer;
+ListNode *buffer;
+ListNode *current;
 unsigned int used_bytes = 0;
+unsigned long lastButtonPress = 0;
 
 void setup() {
     pinMode(SHIFT_DATA, OUTPUT);
     pinMode(SHIFT_CLK, OUTPUT);
     pinMode(SHIFT_LATCH, OUTPUT);
     pinMode(WRITE_EN, OUTPUT);
+    pinMode(WRITE_BUTTON, INPUT);
 
     Serial.begin(57600);
-    Serial.println((int)sizeof(size_t));
-    buffer = (uint8_t *) calloc(EEPROM_SIZE, sizeof(uint8_t));
-    purgeBuffer(FULL_WIPE);
+
+    buffer = (ListNode *) malloc(sizeof(ListNode));
+    buffer->value = 0;
+    buffer->next = NULL;
+    current = buffer;
+
+    ListNode * curr = buffer;
+    // Build linked list
+    for (unsigned long i = 0; i < EEPROM_SIZE; i++) {
+        curr->next = (ListNode *) malloc(sizeof(ListNode));
+        curr -> value = 0;
+        curr = curr->next;
+        // Serial.println(i);
+    }
+
+
+    // purgeBuffer(FULL_WIPE);
     Serial.println("READY\nWaiting for data...");
 }
 
+// Monitors write button
 void loop() {
+    int bState = analogRead(WRITE_BUTTON);
     
+    if (bState > BUTTON_THRESHOLD) {
+        // 3 second cooldown after pressing button
+        if (millis() -  lastButtonPress > 3000) {
+            write();
+
+            // timestamp this button press for cooldown
+            lastButtonPress = millis();
+        }
+    }
+    
+
 }
 
 boolean write() {
-    digitalWrite(WRITE_EN, HIGH);
-
+    // digitalWrite(WRITE_EN, HIGH);
+    ListNode * curr = buffer;
+    while (curr->value != 0 && curr != NULL) {
+        char str[4];
+        sprintf(str,"0x%X", curr->value);
+        Serial.print(str);
+        Serial.print(", ");
+        curr = curr->next;
+    }
+    return false;
 }
 
 void serialEvent() {
-    Serial.print("Used ");
-    Serial.print(used_bytes);
-    Serial.print(" bytes out of  ");
-    Serial.print(EEPROM_SIZE);
-    while (Serial.available() && used_bytes < EEPROM_SIZE) {
-        buffer[used_bytes] = (char)Serial.read();
-        Serial.print(buffer[used_bytes]);
+    while (Serial.available() && current != NULL) {
+        current->value = (char)Serial.read();
+        Serial.print(current->value);
         Serial.print(", ");
+        current = current->next;
         used_bytes++;
     }
 }
@@ -59,31 +101,19 @@ void serialEvent() {
 // Selectively clears data buffer. fullWipe flag specifies
 // if all of the buffer should be cleared (for initialisation)
 // or just the number of bytes which were last used in a read.
-void purgeBuffer(bool fullWipe) {
-    if (fullWipe) {
-        for (unsigned long i = 0; i < EEPROM_SIZE; i++) {
-            buffer[i] = 0;
-        }
-    } else {
-        if (used_bytes < 0) {
-            Serial.println("Failed to purge buffer - no bytes used.");
-        }
-        for (unsigned long i = 0; i < used_bytes - 1 && i < EEPROM_SIZE; i++) {
-            buffer[i] = 0;
-        }
-    }
-    used_bytes = 0;
-    Serial.println("Successfully purged buffer.");
-}
-
-char charFormat(char c) {
-    // check for bad input
-    if (c > 16) {
-        return '!';
-    }
-
-    if (c >= 10) {
-        return c + ASCII_A_OFFSET;
-    }
-    return c + ASCII_ZERO_OFFSET;
-}
+// void purgeBuffer(bool fullWipe) {
+//     if (fullWipe) {
+//         for (unsigned long i = 0; i < EEPROM_SIZE; i++) {
+//             buffer[i] = 0;
+//         }
+//     } else {
+//         if (used_bytes < 0) {
+//             Serial.println("Failed to purge buffer - no bytes used.");
+//         }
+//         for (unsigned long i = 0; i < used_bytes - 1 && i < EEPROM_SIZE; i++) {
+//             buffer[i] = 0;
+//         }
+//     }
+//     used_bytes = 0;
+//     Serial.println("Successfully purged buffer.");
+// }
